2f994be4c32cb4183aace4c44833f5b21c156c14
Luke <lbollam@gmail.com>
2020-11-09 23:08:04 +0800

    transformer model working

--- minst_train.py
+++ minst_train.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 from models.set_prior import SetPrior
+from models.transformer import Encoder
 from models.fspool import FSPool
 from models.size_predictor import SizePredictor
 from tools import AttrDict, Every
 from datasets.mnist_set import MnistSet
 from models.functions.chamfer_distance import chamfer_distance
-from models.set_transformer import STEncoder
 
 
 def set_config():
@@ -41,8 +41,9 @@ class Tspn:
         self._size_pred = SizePredictor(self._c.size_pred_width)
         self._encoder = FSPool(self.element_size, self._c.fspool_n_pieces)
         self._prior = SetPrior(self.element_size)
-        self._transformer = STEncoder(self._c.transformer_depth, self._c.attn_size, self._c.num_heads)
-        self._optimiser = tf.optimizers.Adam(self._c.learning_rate)
+        self._transformer = Encoder(self._c.transformer_depth, self._c.attn_size, self._c.num_heads,
+                                    self._c.dense_size, dataset.element_size)
+        self.optimiser = tf.optimizers.Adam(self._c.learning_rate)
 
     def train(self):
         self.train_ds = self.train_ds.batch(self._c.batch_size)
@@ -79,18 +80,18 @@ class Tspn:
             sampled_elements_conditioned = tf.concat([padded_samples, encoded_shaped], 2)
             mask = tf.cast(tf.math.logical_not(tf.sequence_mask(sizes, self.max_set_size)), tf.float32)
             mask = mask[:, tf.newaxis, tf.newaxis, :]   # mask shape for transformer is (batch_size, 1, 1, seq_len)
-            pred_set = self._transformer(sampled_elements_conditioned, True, mask)
+            pred_set = self._transformer(sampled_elements_conditioned, mask)
 
             # although the arrays contain padded values, chamfer loss is a sum over elements so it wont effect loss
             dist = chamfer_distance(x, pred_set)
             model_loss = tf.reduce_mean(dist, axis=0)
 
         prior_grads = prior_tape.gradient(prior_loss, self._prior.trainable_weights)
-        self._optimiser.apply_gradients(zip(prior_grads, self._prior.trainable_weights))
+        self.optimiser.apply_gradients(zip(prior_grads, self._prior.trainable_weights))
 
         model_trainables = self._encoder.trainable_weights + self._transformer.trainable_weights
         model_grads = model_tape(model_loss, model_trainables)
-        self._optimiser.apply_gradients(zip(model_grads, model_trainables))
+        self.optimiser.apply_gradients(zip(model_grads, model_trainables))
 
 
     def train_size_predictor_step(self, x, sizes):
--- models/transformer.py
+++ models/transformer.py
@@ -69,7 +69,7 @@ class MultiHeadAttention(tf.keras.layers.Layer):
 
 
 class EncoderLayer(tf.keras.layers.Layer):
-    def __init__(self, d_model, num_heads, dff, rate=0.1):
+    def __init__(self, d_model, num_heads, dff):
         super(EncoderLayer, self).__init__()
 
         self.mha = MultiHeadAttention(d_model, num_heads)
@@ -78,38 +78,37 @@ class EncoderLayer(tf.keras.layers.Layer):
         self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
         self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
 
-    def call(self, x, training, mask):
+    def call(self, x, mask):
         attn_output, _ = self.mha(x, x, x, mask)  # (batch_size, input_seq_len, d_model)
-        attn_output = self.dropout1(attn_output, training=training)
         out1 = self.layernorm1(x + attn_output)  # (batch_size, input_seq_len, d_model)
 
         ffn_output = self.ffn(out1)  # (batch_size, input_seq_len, d_model)
-        ffn_output = self.dropout2(ffn_output, training=training)
         out2 = self.layernorm2(out1 + ffn_output)  # (batch_size, input_seq_len, d_model)
 
         return out2
 
 
 class Encoder(tf.keras.layers.Layer):
-    def __init__(self, num_layers, d_model, num_heads, dff, rate=0.1):
+    def __init__(self, num_layers, d_model, num_heads, dff, element_size):
         super(Encoder, self).__init__()
 
         self.d_model = d_model
         self.num_layers = num_layers
 
-        self.enc_layers = [EncoderLayer(d_model, num_heads, dff, rate)
+        self.embedding = tf.keras.layers.Dense(d_model)
+        self.enc_layers = [EncoderLayer(d_model, num_heads, dff)
                            for _ in range(num_layers)]
+        self.out_ff = tf.keras.layers.Dense(element_size)
 
-        self.dropout = tf.keras.layers.Dropout(rate)
-
-    def call(self, x, training, mask):
-        seq_len = tf.shape(x)[1]
-
+    def call(self, x, mask):
         # adding embedding and position encoding.
+        x = self.embedding(x)
         x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
-        x = self.dropout(x, training=training)
 
+        for i in range(self.num_layers):
+            x = self.enc_layers[i](x, mask)
 
+        x = self.out_ff(x)
 
         return x  # (batch_size, input_seq_len, d_model)
 
